a3b519cb47c4b8b0f251157bd02aa35c28bbf6a2
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2019-02-23 20:41:06 +0100

    Add simple input form

---
+++ src/Main.elm
@@ -0,0 +1,56 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { guessInput : String
+    }
+
+
+init : Model
+init =
+    { guessInput = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ChangeGuess String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeGuess newGuess ->
+            { model | guessInput = newGuess }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input
+            [ placeholder "Your Guess"
+            , value model.guessInput
+            , onInput ChangeGuess
+            ]
+            []
+        , div [] [ text model.guessInput ]
+        ]
